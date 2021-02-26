31e0898fff72863611517d3183818f1278727c6b
Claudia Doñate <claudia.do.gallego@gmail.com>
2021-02-26 20:57:05 +0100

    pokemon service created and pokedexList

--- pokemon-client/src/app/app.module.ts
+++ pokemon-client/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { TrainerComponent } from './trainer/trainer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ pokemon-client/src/app/services/pokemon-api.service.ts
@@ -0,0 +1,158 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonApiService {
+
+  constructor(
+  private http: HttpClient,
+  ){ }
+
+  getAllPokemons():Observable<InterfaceList> {
+    return this.http.get<InterfaceList>('https://pokeapi.co/api/v2/pokemon?limit=20&offset=7');
+  }
+
+  getAPokemon(url:string):Observable<InterfacePokemon> {
+    return this.http.get<InterfacePokemon>(url);
+  }
+}
+
+
+interface InterfaceList{
+  next:string,
+  previous: string,
+  results: [
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },  
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },  
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    },
+    {
+      name:string,
+      url: string, 
+    }
+  ]
+}
+
+
+interface InterfacePokemon{
+  id:number,
+  name:string,
+  
+  sprites: {  
+    front_default: string,
+  },
+  stats: [
+    {
+      base_stat:number,
+        // "name": "hp",
+    },
+    {
+      base_stat:number,
+      // "name": "attack",
+    },
+    {
+      base_stat:number,
+        // "name": "defense",
+    },
+    {
+      base_stat:number,
+        // "name": "special-attack",
+    },
+    {
+      base_stat:number,
+       // "name": "special-defense",
+    },
+    {
+      base_stat:number,
+        // "name": "speed", 
+    }
+  ],
+  types: [
+    {
+      type: {
+        name: string,
+       }
+    },
+    {
+      type: {
+        name: string,
+       }
+    }
+  ],
+}
