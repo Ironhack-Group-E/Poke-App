b4eddf88c61aca44e20615ffde83cf6001b97b02
Alejandro <amnaredo@gmail.com>
2021-02-27 17:04:20 +0100

    Added team component and model class

---
+++ pokemon-client/src/app/models/Team/team.ts
@@ -0,0 +1,29 @@
+import { Pokemon } from "../Pokemon/pokemon";
+import { Trainer } from "../Trainer/trainer";
+
+export class Team {
+    constructor(
+        private _trainer: Trainer = new Trainer(0, '', 0, '', ''),
+        private _pokemonList: Pokemon[] = []
+    ) { }
+
+    public get trainer(): Trainer {
+        return this._trainer;
+    }
+
+    public get pokemonList(): Pokemon[] {
+        return this._pokemonList;
+    }
+
+    public set trainer(trainer: Trainer) {
+        this._trainer = trainer;
+    }
+
+    public set pokemonList(pokemonList: Pokemon[]) {
+        this._pokemonList = pokemonList;
+    }
+
+    public addPokemon(pokemon: Pokemon) {
+        this.pokemonList.push(pokemon);
+    }
+}
